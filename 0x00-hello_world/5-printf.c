d7af989c172f0b2653a810b09a0432a099fc4a95
johnifemezuo <johnifemezuoanayo@gmail.com>
2022-09-11 06:00:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  *
  * Return Always 0 (Success)
  */
-int main(void)// this specifies there are no arguments taken by main
+int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
