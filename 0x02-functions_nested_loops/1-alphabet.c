0e5612192c05a50f52252b61a1da50eccd90339f
johnifemezuo <johnifemezuoanayo@gmail.com>
2022-09-18 14:30:30 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int print_alphabet(void)
+{
+	int i = 'a';
+	while (i < 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,6 @@
 
 int _putchar(char c);
 
+int print_alphabet(void);
+
 #endif /*MAIN_H */
