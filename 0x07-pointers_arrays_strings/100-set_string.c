6f115ead6221b39bbf852621a42d29bfad782a19
johnifemezuo <johnifemezuoanayo@gmail.com>
2022-09-26 16:25:08 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - a function
+ * @s: double pointer
+ * @to: pointer to the
+ * Return: Description
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
