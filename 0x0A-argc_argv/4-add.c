225b1d4936827cdc52ca6f072a0af31d8828751d
johnifemezuo <johnifemezuoanayo@gmail.com>
2022-10-02 02:40:08 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,6 +12,7 @@
 int _isnumber(char *s)
 {
 	int i, check, d;
+
 	i = 0, d = 0, check = 1;
 	if (*s == '-')
 		i++;
@@ -36,7 +37,7 @@ int _isnumber(char *s)
 int main(int argc, char **argv)
 {
 	int i, n, ex;
-	
+
 	ex = 0, n = 0;
 	if (argc > 1)
 	{
